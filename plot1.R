9b830538184e5b7aacd98cfcffad398394264ada
rcostenaro <rcostenaro@yahoo.com.br>
2016-05-29 15:22:53 -0300

    Update plot1.R
    Convert data type, generate a histogram, save it to a png file.

--- plot1.R
+++ plot1.R
@@ -3,3 +3,18 @@
 dados <- read.table("household_power_consumption.txt", sep=";", header=T)
 dados$Date <- as.Date(dados$Date, "%d/%m/%Y")
 dados = subset(dados, Date == "2007-02-01" | Date == "2007-02-02")
+
+# CONVERT DATA TYPES
+# To generate a histogram of a data column, the data type must be 'nuneric'
+dados$Global_active_power <- as.numeric(as.character(dados$Global_active_power))
+
+# GENERATE HISTOGRAM
+# color (col) = red
+# title (main) = "Global Active Power"
+# X axis label (xlab) = "Global Active Power (kilowatts)"
+hist(dados$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+# SAVE TO PNG FILE
+# save to a PNG file with a width of 480 pixes and height of 480 pixels
+dev.copy(png, file="plot1.png", width=480, height=480)
+
